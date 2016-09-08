666016e0715ef7b1263ec4872e5ca63a7be56ecc
bradmbaker <bradmbak@gmail.com>
2016-09-08 12:32:16 -0700

    making widget big enough for comments

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ $( document ).ready(function() {
             $("#" + div_id).addClass( blog_post_month );
           } else if( this.textContent.includes('api.soundcloud.com')) {
             $("#" + div_id).append(
-              '<iframe width="100%" height="120" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//' + this.textContent + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false"></iframe>'
+              '<iframe width="100%" height="160" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//' + this.textContent + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false"></iframe>'
             )
           } else {
             $("#" + div_id).append( this ) ;  
